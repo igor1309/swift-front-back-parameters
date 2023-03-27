addd4f8d0ad5151a5e27b14aef57af9f6d5b9bc8
Igor Malyarov <igor1309@gmail.com>
2023-03-27 16:27:09 +0300

    example

--- Package.swift
+++ Package.swift
@@ -1,28 +1,104 @@
 // swift-tools-version: 5.7
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "swift-front-back-parameters",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "swift-front-back-parameters",
             targets: ["swift-front-back-parameters"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .tagged
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "swift-front-back-parameters",
-            dependencies: []),
-        .testTarget(
-            name: "swift-front-back-parametersTests",
-            dependencies: ["swift-front-back-parameters"]),
+            dependencies: [
+                .tagged,
+            ]
+        ),
     ]
 )
+
+// MARK: - Point-Free
+
+private extension Package.Dependency {
+    
+    static let casePaths = Package.Dependency.package(
+        url: .pointFreeGitHub + .case_paths,
+        from: .init(0, 10, 1)
+    )
+    static let combineSchedulers = Package.Dependency.package(
+        url: .pointFreeGitHub + .combine_schedulers,
+        from: .init(0, 9, 1)
+    )
+    static let identifiedCollections = Package.Dependency.package(
+        url: .pointFreeGitHub + .swift_identified_collections,
+        from: .init(0, 4, 1)
+    )
+    static let snapshotTesting = Package.Dependency.package(
+        url: .pointFreeGitHub + .swift_snapshot_testing,
+        from: .init(1, 10, 0)
+    )
+    static let swiftUINavigation = Package.Dependency.package(
+        url: .pointFreeGitHub + .swiftui_navigation,
+        from: .init(0, 4, 5)
+    )
+    static let tagged = Package.Dependency.package(
+        url: .pointFreeGitHub + .swift_tagged,
+        from: .init(0, 7, 0)
+    )
+}
+
+private extension Target.Dependency {
+    
+    static let casePaths = product(
+        name: .casePaths,
+        package: .case_paths
+    )
+    static let combineSchedulers = product(
+        name: .combineSchedulers,
+        package: .combine_schedulers
+    )
+    static let identifiedCollections = product(
+        name: .identifiedCollections,
+        package: .swift_identified_collections
+    )
+    static let snapshotTesting = product(
+        name: .snapshotTesting,
+        package: .swift_snapshot_testing
+    )
+    static let swiftUINavigation = product(
+        name: .swiftUINavigation,
+        package: .swiftui_navigation
+    )
+    static let tagged = product(
+        name: .tagged,
+        package: .swift_tagged
+    )
+}
+
+private extension String {
+    
+    static let pointFreeGitHub = "https://github.com/pointfreeco/"
+    
+    static let casePaths = "CasePaths"
+    static let case_paths = "swift-case-paths"
+    
+    static let combineSchedulers = "CombineSchedulers"
+    static let combine_schedulers = "combine-schedulers"
+    
+    static let identifiedCollections = "IdentifiedCollections"
+    static let swift_identified_collections = "swift-identified-collections"
+    
+    static let snapshotTesting = "SnapshotTesting"
+    static let swift_snapshot_testing = "swift-snapshot-testing"
+    
+    static let swiftUINavigation = "SwiftUINavigation"
+    static let swiftui_navigation = "swiftui-navigation"
+    
+    static let tagged = "Tagged"
+    static let swift_tagged = "swift-tagged"
+}
--- Sources/swift-front-back-parameters/swift_front_back_parameters.swift
+++ Sources/swift-front-back-parameters/swift_front_back_parameters.swift
@@ -1,6 +1,67 @@
-public struct swift_front_back_parameters {
-    public private(set) var text = "Hello, World!"
+// я подумал что это отличный кейс для `Strong ID Type`, который делается с помощью фантомных типов
+// и тут снова горячо рекомендую Point-Free 😃 с их библиотекой [Tagged](https://github.com/pointfreeco/swift-tagged) (с нее, кстати, началось мое знакомство с ними)
+// используя фантомные типы мы никогда не смешаем id разных сущностей просто потому что это совершенно разные типы
 
-    public init() {
+import Tagged
+
+protocol Parameter<ID, Value> {
+    
+    associatedtype ID
+    associatedtype Value
+    
+    var id: Tagged<Value, ID> { get }
+    var value: Value { get }
+}
+
+struct InfoParameter: Parameter {
+    
+    let id: Tagged<Info, String>
+    let value: Info
+    
+    struct Info {
+        
+        let title: String
+        let icon: Icon
+    }
+}
+
+struct Icon {
+    
+    //...
+}
+
+struct ProductParameter: Parameter {
+    
+    let id: Tagged<Product, String>
+    let value: Product
+    
+    struct Product {
+        
+        let type: ProductType
+        
+        enum ProductType {
+            
+            case card
+            case account
+            // ...
+        }
+    }
+}
+
+struct ButtonParameter: Parameter {
+    
+    let id: Tagged<Button, String>
+    let value: Button
+    
+    struct Button {
+        
+        let title: String
+        let style: ButtonStyle
+        
+        enum ButtonStyle {
+            
+            case primary
+            case secondary
+        }
     }
 }
